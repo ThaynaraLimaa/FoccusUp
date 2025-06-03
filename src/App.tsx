06b052d33498c7efef98eba2cc417209760ce130
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-06-03 11:53:18 -0300

    update App with new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Timer from "./components/timer/Timer"
+import Header from "./layout/Header"
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <Timer />
     </>
   )
 }
