b323a60d43a5481ffe45af7d6b80bcd938c2b794
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-06-05 13:17:13 -0300

    add functionality to save username on localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import Timer from "./components/timer/Timer"
 import Header from "./layout/Header"
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
+  const [username, setUsername] = useLocalStorage('foccusUsername', '')
+
+  if (!username) {
+    const prompt = window.prompt(`What's your name?`);
+    if (!prompt) {
+      window.alert("Alright, I'm going to call you Tony Stank then");
+      setUsername('Tony Stank');
+    } else {
+      setUsername(prompt);
+    }
+  }
 
   return (
     <>
