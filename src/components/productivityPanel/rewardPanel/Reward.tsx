0c6e59a1d39bea24fc8ae55c13837e9462d33cc5
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-06-09 20:10:14 -0300

    add rewards panel

---
+++ src/components/productivityPanel/rewardPanel/Reward.tsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from './Reward.module.css'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+
+type Reward = {
+    id: string,
+    name: string,
+    cost: number,
+    isCollected: boolean
+}
+
+export default function Reward({ id, name, cost, isCollected }: Reward) {
+    const [isChecked, setIsChecked] = useState(isCollected);
+
+    const handleChangeCollectedState = () => {
+        setIsChecked(prev => !prev);
+        // more actions will happen
+    }
+
+    const handleDeleteReward = () => {
+        alert("I'm still working on this, this reward won't be deleted for now")
+    }
+
+    return (
+        <li className={styles.reward}>
+            <label htmlFor={`${id}-checkbox`} className={styles.label}>
+                <input
+                    type="checkbox"
+                    name={`${id}-checkbox`}
+                    id={`${id}-checkbox`}
+                    className={styles.checkbox}
+                    checked={isChecked}
+                    onChange={handleChangeCollectedState}
+                />
+                <span className={styles.customCheckbox}></span>
+                {name}
+            </label>
+            <div className={styles.left}>
+                <p aria-label={`reward cost: ${cost}`} className={styles.cost}>{cost} CDR</p>
+                <button className={styles.deleteButton} aria-label={`delete reward ${name}`} onClick={handleDeleteReward}><FontAwesomeIcon icon={faTrash} /></button>
+            </div>
+
+        </li>
+    )
+}
---
+++ src/components/productivityPanel/rewardPanel/RewardsPanel.tsx
@@ -0,0 +1,24 @@
+import Reward from './Reward';
+import styles from './RewardsPanel.module.css'
+
+const rewardTempList = [
+    { id: '10', name: "Watch a Movie", cost: 6, isCollected: false },
+    { id: '20', name: "Order a Special Dessert", cost: 6, isCollected: true },
+    { id: '30', name: "Buy a Small Gift", cost: 6, isCollected: false }
+];
+
+export default function RewardsPanel() {
+    return (
+        <div className={styles.rewardsPanelContainer}>
+            <div className={styles.header}>
+                <h2>My Rewards</h2>
+                <span>Today's Credits: 12 CDR</span>
+            </div>
+            <ul className={styles.taskListContainer}>
+                {rewardTempList.map(reward => (
+                    <Reward {...reward} key={reward.id}/>
+                ))}
+            </ul>
+        </div>
+    )
+}
