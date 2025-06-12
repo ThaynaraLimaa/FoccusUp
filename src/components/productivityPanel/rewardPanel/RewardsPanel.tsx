efb630caafdd38a2a2f7e6c934cbf6e407bd6796
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-06-12 15:03:24 -0300

    add button to RewardsPanel to add new reward

--- src/components/productivityPanel/rewardPanel/RewardsPanel.tsx
+++ src/components/productivityPanel/rewardPanel/RewardsPanel.tsx
@@ -1,5 +1,8 @@
+import { FormEvent, useRef, useState } from 'react';
 import Reward from './Reward';
 import styles from './RewardsPanel.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const rewardTempList = [
     { id: '10', name: "Watch a Movie", cost: 6, isCollected: false },
@@ -13,6 +16,14 @@ type RewardsPanelProps = {
 }
 
 export default function RewardsPanel({ panelIndex, selectedIndex }: RewardsPanelProps) {
+    const [isAdding, setIsAdding] = useState(false); 
+    const rewardRef = useRef<HTMLInputElement>(null); 
+
+    const handleSubmitReward = (e: FormEvent) => {
+        e.preventDefault();
+        alert("I'm working on this, this reward won't be added for now")
+    }
+
     return (
         <div
             className={styles.rewardsPanelContainer}
@@ -31,6 +42,24 @@ export default function RewardsPanel({ panelIndex, selectedIndex }: RewardsPanel
                     <Reward {...reward} key={reward.id} />
                 ))}
             </ul>
+            
+            {isAdding && (
+                <form className={styles.newRewardForm} onSubmit={handleSubmitReward}>
+                    <label htmlFor="newTaskName" className={styles.label}>
+                        Reward
+                        <input type="text" id='newTaskName' className={styles.input} required ref={rewardRef} />
+                    </label>
+
+                    <div className={styles.formActions}>
+                        <button className={styles.submitBtn} type='submit'>Add</button>
+                        <button className={styles.cancelBtn} onClick={() => setIsAdding(false)}>Cancel</button>
+                    </div>
+                </form>
+            )}
+
+            <button className={styles.addRewardBtn} onClick={() => setIsAdding(true)} style={{ display: isAdding ? 'none' : 'block' }}>
+                <FontAwesomeIcon icon={faPlus} /> Add New Reward
+            </button>
         </div>
     )
 }
