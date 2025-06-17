9ec00d782b252363cbec2a144accdab4638f1543
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-06-17 16:43:50 -0300

    add dynamic stats to day progress

--- src/components/productivityPanel/dayProgressPanel/DayProgressPanel.tsx
+++ src/components/productivityPanel/dayProgressPanel/DayProgressPanel.tsx
@@ -1,12 +1,7 @@
+import { useContext } from 'react';
 import styles from './DayProgressPanel.module.css'
-
-const tempProgress = {
-    hours: 4,
-    cycles: 8,
-    CDR: { earned: 12, spent: 6 },
-    redeemedRewards: 1,
-    tasks: { completed: 3, total: 5 }
-}
+import { DayInformationContext } from '../../../context/DayInformationContext';
+import { TasksContext } from '../../../context/TasksContext';
 
 type DayProgressPanelProps = {
     panelIndex: number,
@@ -14,9 +9,11 @@ type DayProgressPanelProps = {
 }
 
 export default function DayProgressPanel({ panelIndex, selectedIndex }: DayProgressPanelProps) {
-    const taskCompletedPercentage = (tempProgress.tasks.completed / tempProgress.tasks.total) * 100;
+    const { totalCircles, totalHours, creditsEarned, creditsSpended, rewardsRedeemed } = useContext(DayInformationContext);
+    const {totalTasks, completedTasks} = useContext(TasksContext)
+    const taskCompletedPercentage = (completedTasks / totalTasks) * 100;
     const now = new Date();
-    const date = now.toLocaleDateString('en-US', { month: 'long', day: '2-digit', year: 'numeric' })
+    const date = now.toLocaleDateString('en-US', { month: 'long', day: '2-digit', year: 'numeric' }); 
 
     return (
         <div
@@ -32,16 +29,16 @@ export default function DayProgressPanel({ panelIndex, selectedIndex }: DayProgr
                 <span>{date}</span>
             </div>
             <ul className={styles.progressNumbersContainer}>
-                <li className={styles.numbers}><span className={styles.number}>{tempProgress.hours}</span>Hours</li>
-                <li className={styles.numbers}><span className={styles.number}>{tempProgress.cycles}</span>Cycles</li>
-                <li className={styles.numbers}><span className={styles.number}>{tempProgress.CDR.earned}</span>CDR Earned</li>
-                <li className={styles.numbers}><span className={styles.number}>{tempProgress.CDR.spent}</span>CDR Spent</li>
-                <li className={styles.numbers}><span className={styles.number}>{tempProgress.redeemedRewards}</span>Rewards Redeemed</li>
+                <li className={styles.numbers}><span className={styles.number}>{totalHours}</span>Hours</li>
+                <li className={styles.numbers}><span className={styles.number}>{totalCircles}</span>Cycles</li>
+                <li className={styles.numbers}><span className={styles.number}>{creditsEarned}</span>CDR Earned</li>
+                <li className={styles.numbers}><span className={styles.number}>{creditsSpended}</span>CDR Spended</li>
+                <li className={styles.numbers}><span className={styles.number}>{rewardsRedeemed}</span>Rewards Redeemed</li>
             </ul>
             <div className={styles.tasksProgressContainer}>
                 <div className={styles.taskProgressHeader}>
                     <p>Tasks</p>
-                    <span>{tempProgress.tasks.completed}/{tempProgress.tasks.total}</span>
+                    <span>{completedTasks}/{totalTasks}</span>
                 </div>
                 <div className={styles.progressBarContainer} role='progressbar' aria-valuenow={taskCompletedPercentage} aria-valuemin={0} aria-valuemax={100} >
                     <div className={styles.progress} style={{ width: `${taskCompletedPercentage}%` }}></div>
