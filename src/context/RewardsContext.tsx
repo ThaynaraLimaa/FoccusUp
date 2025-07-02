68017e179c109f79f88518fd6673588d33b2d9aa
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-07-02 10:08:20 -0300

    add reward context for better management

---
+++ src/context/RewardsContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, ReactNode} from "react";
+import { Reward } from "../types/rewards";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { LocalStorageKeys } from "../constants/localStorageKeys";
+import { v4 as uuid4 } from "uuid";
+
+type RewardsContextType = {
+    rewards: Reward[],
+    addReward: (name: string) => void
+    toogleCollectReward: (id: string) => Reward | undefined
+    deleteReaward: (id: string) => void
+}
+
+export const RewardsContext = createContext({} as RewardsContextType);
+
+export function RewardsProvider({ children }: { children: ReactNode }) {
+    const [rewards, setRewards] = useLocalStorage(LocalStorageKeys.Reward, [] as Reward[]);
+
+    const addReward = (name: string) => {
+        setRewards(prev => [
+            ...prev,
+            createNewReward(name)
+        ])
+    }
+
+    const toogleCollectReward = (id: string) => {
+        const updatedRewards = rewards.map(reward => {
+            return reward.id === id ? { ...reward, isCollected: !reward.isCollected } : reward
+        })
+
+        setRewards(updatedRewards)
+        return updatedRewards.find(reward => reward.id === id); // it must return to check later if is it is "spending" or "returning" action
+    }
+
+    const deleteReaward = (id: string) => {
+        setRewards(prev => {
+            return prev.filter(reward => reward.id !== id)
+        })
+    }
+
+    return (
+        <RewardsContext.Provider value={{ rewards, addReward, toogleCollectReward, deleteReaward }}>
+            {children}
+        </RewardsContext.Provider>
+    )
+}
+
+function createNewReward(name: string): Reward {
+    return {
+        id: uuid4(),
+        name: name,
+        cost: 6,
+        isCollected: false
+    }
+}
