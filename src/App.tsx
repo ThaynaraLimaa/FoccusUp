3810d70c7c599a57f9e25da56e58e03cee8f06ce
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-07-07 12:52:46 -0300

    update app to new foccusUpInformation format

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,58 @@ import ProductivityPanel from "./components/productivityPanel/ProductivityPanel"
 import { LocalStorageKeys } from "./constants/localStorageKeys";
 import { DayInformationProvider } from "./context/DayInformationContext";
 import { TasksProvider } from "./context/TasksContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FoccusUpInformations } from "./types/foccusUpInformation";
+import { RewardsProvider } from "./context/RewardsContext";
 
 export type TimerState = 'stopped' | 'running' | 'paused'
 
 function App() {
-  const [username, setUsername] = useLocalStorage(LocalStorageKeys.Username, '')
+  const [foccusUpInformation, setFoccusUpInformation] = useLocalStorage(LocalStorageKeys.FoccusUpInformations, {} as FoccusUpInformations);
   const [timerState, setTimerState] = useState<TimerState>('stopped');
 
-  if (!username) {
+  if (!foccusUpInformation.username) {
     const prompt = window.prompt(`What's your name?`);
     if (!prompt) {
       window.alert("Alright, I'm going to call you Tony Stank then");
-      setUsername('Tony Stank');
+      setFoccusUpInformation(prev => {
+        return {
+          ...prev,
+          username: 'Tony Stank'
+        }
+      });
     } else {
-      setUsername(prompt);
+      setFoccusUpInformation(prev => {
+        return {
+          ...prev,
+          username: prompt
+        }
+      });
     }
   }
+
+  useEffect(() => {
+    // If there isn't a last use date, it is created with the current date 
+    if (!foccusUpInformation.currentDate) {
+      setFoccusUpInformation(prev => {
+        return {
+          ...prev,
+          currentDate: `${new Date('Apr 05, 2025').toDateString()}`
+        }
+      })
+    }
+
+  }, [])
 
   return (
     <>
       <Header />
       <DayInformationProvider>
         <TasksProvider>
-          <Timer username={username} timerState={timerState} setTimerState={setTimerState} />
-          {timerState == "stopped" && <ProductivityPanel />}
+          <RewardsProvider>
+            <Timer username={foccusUpInformation.username} timerState={timerState} setTimerState={setTimerState} />
+            {timerState == "stopped" && <ProductivityPanel />}
+          </RewardsProvider>
         </TasksProvider>
       </DayInformationProvider>
     </>
